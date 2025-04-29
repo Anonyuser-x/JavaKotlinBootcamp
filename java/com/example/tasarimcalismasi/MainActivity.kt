eac52f2b01c18cd0554124845f40afa486fba9e9
Anonyuser-x <kemalbaz10@gmail.com>
2025-04-29 21:51:04 +0300

    tasarım ve logo yaptık

---
+++ java/com/example/tasarimcalismasi/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.tasarimcalismasi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // R -> res klasörüdür , kaynaklar demektir, ama kotlin dosyaları içindedir
+        // @ -> işareti xml dosyaları içinde res klasörü kaynaklar demektir
+        // <item name="android:statusBarColor">@color/anaRenk</item>
+
+    }
+}
